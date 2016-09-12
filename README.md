b2a70b57141658dbaf4be9807bb6049b3f78091f
josef leyva <jalm_x@hotmail.com>
2016-09-12 18:14:18 -0500

    Hecho

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nte
 Calcula la equivalencia en función de número introducido 
+
+![vista](./vista.png "Vista")
--- app/src/main/java/com/leyva/josef/nto/MainActivity.java
+++ app/src/main/java/com/leyva/josef/nto/MainActivity.java
@@ -1,13 +1,59 @@
 package com.leyva.josef.nto;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Declarando los objetos UI
+     */
+    private EditText calficacion, reactivos;
+    private TextView resultado;
+    private Button calcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    resultado.setText("");
+                    double margen = 0.5;
+                    int c = Integer.parseInt(calficacion.getText().toString()); //calificacion
+                    int r = Integer.parseInt(reactivos.getText().toString()); //reactivos
+
+                    for (int i = r * 2; i >= 0; i--) {
+                        double rs = ((r - i * margen) * c) / r;
+                        resultado.append(String.format("Reactivo: %.2f", r - i * margen) + " - " +
+                                String.format("Resultado: %.2f", rs) + "\n");
+                    }
+
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Error - Sólo digitos", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
+
+    /**
+     * Inicializando parametros
+     */
+    private void init() {
+        calficacion = (EditText) findViewById(R.id.calificacion);
+        reactivos = (EditText) findViewById(R.id.reactivos);
+        resultado = (TextView) findViewById(R.id.resultado);
+        calcular = (Button) findViewById(R.id.calcular);
+    }
+
 }
