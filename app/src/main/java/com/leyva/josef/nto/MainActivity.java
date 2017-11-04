e404585fd78e0622cda81cd44bfb0588e72dd6d3
Josef Leyva <jalm_x@hotmail.com>
2017-11-04 11:08:43 -0600

    Refactoring, lenguage

--- app/src/main/java/com/leyva/josef/nto/MainActivity.java
+++ app/src/main/java/com/leyva/josef/nto/MainActivity.java
@@ -8,48 +8,75 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.leyva.josef.nto.util.Calculate;
+import java.util.ArrayList;
+
+import static com.leyva.josef.nto.util.Calculate.ruleTre;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * Declarando los objetos UI
-     */
-    private EditText calficacion, reactivos;
-    private TextView resultado;
-    private Button calcular;
+    // TODO: 11/3/17 ARREGLAR LO DE LA INTERFAZ
+    // TODO: 11/3/17 strings de idiomas
+    // TODO: 11/3/17 al dar click ocultar el teclado
+    // TODO: 11/3/17 crear ramas
+    // TODO: 11/3/17 refactoring 
+    // TODO: 11/3/17 agregar ads
+    // TODO: 11/3/17 crear la activity para los datos
+
+    // TODO: 11/3/17 a futuro, agregar opciones de 1/4, 1/2 incremento
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
+        buttonAction();
+    }
+
+    private void buttonAction() {
+        Button calcular = findViewById(R.id.calcular);
+        final TextView resultado = findViewById(R.id.resultado);
 
         calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 resultado.setText("");
-
-                try {
-                    resultado.setText(
-                            Calculate.calculate(calficacion.getText().toString(), reactivos.getText().toString())
-                    );
-
-                } catch (Exception e) {
-                    Toast.makeText(MainActivity.this, R.string.message_error, Toast.LENGTH_SHORT).show();
-                }
+                resultado.append(textDisplay());
             }
         });
     }
 
-    /**
-     * Inicializando parametros
-     */
-    private void init() {
-        calficacion = (EditText) findViewById(R.id.calificacion);
-        reactivos = (EditText) findViewById(R.id.reactivos);
-        resultado = (TextView) findViewById(R.id.resultado);
-        calcular = (Button) findViewById(R.id.calcular);
+    private ArrayList<Double> params() {
+        ArrayList<Double> values = new ArrayList<>();
+        EditText calficacion = findViewById(R.id.calificacion);
+        EditText reactivos = findViewById(R.id.reactivos);
+
+        try {
+            double c = Double.parseDouble(calficacion.getText().toString()); //calificacion
+            double r = Double.parseDouble(reactivos.getText().toString()); //reactivos
+            values.add(c);
+            values.add(r);
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, R.string.message_error, Toast.LENGTH_SHORT).show();
+        }
+
+        return values;
     }
 
+    private StringBuilder textDisplay() {
+        StringBuilder resultado = new StringBuilder();
+        float margen = 0.5f;
+        ArrayList<Double> values = params();
+
+        for (int i = (int) (values.get(1) * 2); i >= 0; i--) {
+
+            double v = ruleTre(values.get(0), values.get(1), margen, i);
+
+            resultado.append(
+                    String.format("%s: %.2f", getText(R.string.reactive), values.get(1) - i * margen) + " - " +
+                            String.format("%s: %.2f", getText(R.string.result), v) + "%\n"
+            );
+        }
+
+        return resultado;
+    }
 }
--- app/src/main/java/com/leyva/josef/nto/util/Calculate.java
+++ app/src/main/java/com/leyva/josef/nto/util/Calculate.java
@@ -9,20 +9,7 @@ public class Calculate {
     private Calculate() {
     }
 
-    public static StringBuilder calculate(String calificacion, String reactivos) {
-        StringBuilder resultado = new StringBuilder();
-
-        double margen = 0.5;
-        int c = Integer.parseInt(calificacion); //calificacion
-        int r = Integer.parseInt(reactivos); //reactivos
-
-        for (int i = r * 2; i >= 0; i--) {
-            double rs = ((r - i * margen) * c) / r;
-            resultado.append(String.format("Reactivo: %.2f", r - i * margen) + " - " +
-                    String.format("Resultado: %.2f", rs) + "\n");
-        }
-
-        return resultado;
-
+    public static double ruleTre(double calificacion, double reactivos, double margen, int posicion) {
+        return ((reactivos - posicion * margen) * calificacion) / reactivos;
     }
 }
